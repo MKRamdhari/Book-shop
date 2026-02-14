beaba4ad4f8a26ec98ec456cdbd779c04c5a8597
Mukesh <coddermk@gmail.com>
2026-02-14 21:23:10 +0530

    add resend API email

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { sendEmail } from "@/lib/mailer";
+//import { sendEmail } from "@/lib/mailer";
+import { sendEmail } from "@/lib/resend";
 import fs from "fs";
 import path from "path";
 import {
